66f5288e517048c6a15be8ce5fcd8bb38c26dc17
Matias Astrain <mastrain@alumnos.uai.cl>
2025-03-20 00:31:59 -0300

    no message

---
+++ app1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include <math.h>
+#include "metricaN.h" //o metrics.h pero prefiero usar el nombre de la instrucción
+#include "utilidades.h" //o utils que aparece en el contenido sugerido
+#include "stucturapedido.h" //Almacenar los datos de los pedidos dados por el csv
---
+++ stucturapedido.h
@@ -0,0 +1,19 @@
+#ifndef STRUCTURAPEDIDO_H
+#define STRUCTURAPEDIDO_H
+
+typedef struct {
+    int pizza_id;
+    int order_id;
+    char pizza_name_id[75];
+    int quantity;
+    char order_date[75];
+    char order_time[75];
+    float unit_price;
+    float total_price;
+    char pizza_size[4];
+    char pizza_category[75];
+    char pizza_ingredients[275];
+    char pizza_name[75];
+}; Pedido;
+
+#endif 
