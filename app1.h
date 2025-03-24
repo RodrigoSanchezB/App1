72a7cc8fa6aa1447c26f225e316fe05897e6ee30
jbesp <163190318+jbesp@users.noreply.github.com>
2025-03-24 12:58:41 -0300

    Update app1.h

--- app1.h
+++ app1.h
@@ -1,7 +1,19 @@
 #ifndef APP_1
 #define APP_1
 
-typedef struct
-
+typedef struct {
+    int pizza_id;
+    int order_id;
+    char *pizza_name_id;
+    int quantity;
+    char *order_date;
+    char *order_time;
+    float unit_price;
+    float total_price;
+    char *pizza_size;
+    char *pizza_category;
+    char *pizza_ingredients;
+    char *pizza_name;
+} order;
 
 #endif
